015474a392212dcccfea1ed19ff095f68ab4b6d0
wiliam lee <wiliam.tae@gmail.com>
2024-04-24 09:37:57 -0300

    safe access to variables and custom error message

--- env/index.ts
+++ env/index.ts
@@ -2,8 +2,17 @@ import 'dotenv/config'
 import { z } from 'zod'
 
 const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_URL: z.string(),
   PORT: z.number().default(3333),
 })
 
-export const env = envSchema.parse(process.env)
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('Invalid envorinment variable!', _env.error?.format())
+
+  throw new Error('Invalid envorinment variables.')
+}
+
+export const env = _env.data
