c1571eb295c3863d331b7666a4b6a48ba1daa771
wiliam lee <wiliam.tae@gmail.com>
2024-04-25 09:27:34 -0300

    feat: add route to get transaction by id

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -3,6 +3,12 @@ import { Knex } from 'knex'
 
 declare module 'knex/types/tables' {
   export interface Tables {
-    transactions: string
+    transactions: {
+      id: string
+      title: string
+      amount: number
+      created_at: string
+      session_id: string
+    }
   }
 }
--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,7 +5,24 @@ import { randomUUID } from 'node:crypto'
 
 export async function transactionsRoutes(app: FastifyInstance) {
   app.get('/', async () => {
-    return await knex('transactions').select('*')
+    const transactions = await knex('transactions').select('*')
+
+    return {
+      transactions,
+    }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return {
+      transaction,
+    }
   })
 
   app.post('/', async (request, reply) => {
