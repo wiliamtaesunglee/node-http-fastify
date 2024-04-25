2a5c05629f01e2c5c96fc074cdce4dd046e5bdfe
wiliam lee <wiliam.tae@gmail.com>
2024-04-25 09:05:12 -0300

    feat: update routes to be on transaction and create the post transaction route

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,19 +1,29 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
+import { z } from 'zod'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    return await knex('transactions').where('amount', 1000).select('*')
+  app.get('/', async () => {
+    return await knex('transactions').select('*')
   })
 
-  app.post('/hello', async () => {
-    return await knex('transactions').insert(
-      {
-        id: crypto.randomUUID(),
-        title: 'Test Transaction',
-        amount: 1000,
-      },
-      '*',
+  app.post('/', async (request, reply) => {
+    const createTransactionBosySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionBosySchema.parse(
+      request.body,
     )
+
+    await knex('transactions').insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
   })
 }
